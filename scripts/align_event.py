a6a463a10fe1688379653bc8cbc002609f604056
Tuan Manh Lai <laituan245@kaist.ac.kr>
2020-10-11 18:35:53 -0500

    [align_event.py] Remove old event file + Rename new event file

--- scripts/align_event.py
+++ scripts/align_event.py
@@ -9,21 +9,21 @@ from argparse import ArgumentParser
 if __name__ == "__main__":
     # Parse argument
     parser = ArgumentParser()
-    parser.add_argument('--output_entity', default='/shared/nas/data/m1/tuanml2/kairos/output/entity.cs')
-    parser.add_argument('--output_event', default='/shared/nas/data/m1/tuanml2/kairos/output/corrected_format/event.cs')
+    parser.add_argument('--input_entity', default='/shared/nas/data/m1/tuanml2/kairos/output/corrected_format/entity.cs')
+    parser.add_argument('--input_event', default='/shared/nas/data/m1/tuanml2/kairos/output/corrected_format/event.cs')
     parser.add_argument('--output_path', default='/shared/nas/data/m1/tuanml2/kairos/output/corrected_format/event_2.cs')
     parser.add_argument('--debug', action='store_true')
     args = parser.parse_args()
 
     # DEBUG Mode
     if args.debug:
-        args.output_entity = 'resources/samples/outputs/entity.cs'
-        args.output_event = 'resources/samples/outputs/event.cs'
+        args.input_entity = 'resources/samples/outputs/entity.cs'
+        args.input_event = 'resources/samples/outputs/event.cs'
         args.output_path = 'resources/samples/outputs/new_event.cs'
 
     # Read output entity file
     mid2eid = {}
-    with open(args.output_entity, 'r', encoding='utf-8') as entity_f:
+    with open(args.input_entity, 'r', encoding='utf-8') as entity_f:
         for line in entity_f:
             es = line.split('\t')
             if es[1].endswith('mention'):
@@ -31,7 +31,7 @@ if __name__ == "__main__":
 
     # Output file
     with open(args.output_path, 'w+', encoding='utf-8') as output_f:
-        with open(args.output_event, 'r', encoding='utf-8') as event_f:
+        with open(args.input_event, 'r', encoding='utf-8') as event_f:
             for line in event_f:
                 line = line.strip()
                 if len(line) == 0: continue
@@ -42,3 +42,7 @@ if __name__ == "__main__":
                     mid = es[-2]
                     es[-3] = mid2eid[mid]
                     output_f.write('{}\n'.format('\t'.join(es)))
+
+    # Delete old file
+    os.remove(args.input_event)
+    os.rename(args.output_path, args.input_event)
