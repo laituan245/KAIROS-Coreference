f77025a2135acf5628dc70d28ea1eb77fec27668
Tuan Lai <boy19ykienandon@yahoo.com.vn>
2020-11-19 13:36:35 -0600

    Update paths

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from coref import coref_main
 from utils import read_cluster_info
 
-ONEIE_OUTPUT = '/shared/nas/data/m1/yinglin8/kairos/result/kairos_all_oct_27/m1_m2'
-LINKING_OUTPUT = '/shared/nas/data/m1/xiaoman6/tmp/20200920_kairos_linking/output/kairos_all_oct_27/m1_m2/linking/en.linking.wikidata.cs'
+ONEIE_OUTPUT = '/shared/nas/data/m1/tuanml2/oneie_with_relation'
+LINKING_OUTPUT = '/shared/nas/data/m1/xiaoman6/tmp/20200920_kairos_linking/output/oneie_with_relation/linking/en.linking.wikidata.cs'
 COREFERENCE_OUTPUT = '/shared/nas/data/m1/tuanml2/tmp/coref/'
 
 if __name__ == "__main__":
