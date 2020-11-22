861456e9542f2a2cbf330998d0a56223ee2bdac7
Tuan Lai <boy19ykienandon@yahoo.com.vn>
2020-11-21 19:45:01 -0600

    Update test folder structure

--- test/end2end/test.py
+++ test/end2end/test.py
@@ -3,15 +3,19 @@ import glob
 import json
 import requests
 
+from os.path import join
+
+BASE_PATH = 'test/end2end'
+
 def read_data(json_file):
     f = open(json_file, 'r')
     data = f.read()
     f.close()
     return data
 
-input_data = read_data('test/sample_input.json')
+input_data = read_data(join(BASE_PATH, 'sample_input.json'))
 input_data = json.loads(input_data)
 
 response = requests.post('http://localhost:20202/process', json={'data': input_data})
-with open('test/sample_response.txt', 'w+') as f:
+with open(join(BASE_PATH, 'sample_response.txt'), 'w+') as f:
     f.write(response.text)
