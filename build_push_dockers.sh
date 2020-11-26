33927cb1cf14fed7883060f40779d4242b5650a6
Tuan Lai <laituan245@kaist.ac.kr>
2020-11-26 17:47:05 -0600

    Updated build_push_dockers.sh

--- build_push_dockers.sh
+++ build_push_dockers.sh
@@ -3,3 +3,5 @@ docker build --tag laituan245/kairos_coref .
 docker push laituan245/kairos_coref
 docker tag laituan245/kairos_coref laituan245/kairos_coref:api
 docker push laituan245/kairos_coref:api
+docker tag laituan245/kairos_coref laituan245/kairos_coref:dev
+docker push laituan245/kairos_coref:dev
