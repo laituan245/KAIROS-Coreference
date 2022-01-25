fd7148055d9ee9ea5be8120b03c80cd067e19c38
Tuan Manh Lai <boy19ykienandon@yahoo.com.vn>
2022-01-24 21:50:32 -0800

    Update run_batch.py

--- run_batch.py
+++ run_batch.py
@@ -3,7 +3,7 @@ import glob
 import json
 import requests
 
-from os.path import isfile, join
+from os.path import isfile, isdir, join
 from argparse import ArgumentParser
 
 def create_dir_if_not_exist(dir):
@@ -21,48 +21,50 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Process each sub input directory
-    for dirname in os.listdir(args.base_input_dir):
-        print(f'Processing: {dirname}')
-        input_dir_path = join(args.base_input_dir, dirname)
-        input_fp = join(input_dir_path, 'coref_input.json')
-        if not isfile(input_fp): continue
-        input_data = read_data(input_fp)
-        input_data = json.loads(input_data)
-        # Call the API
-        response = requests.post('http://localhost:20202/process', json={'data': input_data})
-        with open(join(input_dir_path, 'sample_response.txt'), 'w+') as f:
-            f.write(response.text)
-        # convert
-        BASE_PATH = input_dir_path
-        EN_BASE_PATH = join(BASE_PATH, 'en')
-        ES_BASE_PATH = join(BASE_PATH, 'es')
-        ALL_BASE_PATH = join(BASE_PATH, 'all')
-        create_dir_if_not_exist(ALL_BASE_PATH)
-        create_dir_if_not_exist(EN_BASE_PATH)
-        create_dir_if_not_exist(ES_BASE_PATH)
+    while True:
+        for dirname in os.listdir(args.base_input_dir):
+            print(f'Processing: {dirname}')
+            input_dir_path = join(args.base_input_dir, dirname)
+            input_fp = join(input_dir_path, 'coref_input.json')
+            if not isfile(input_fp): continue
+            if isdir(join(input_dir_path, 'all')): continue
+            input_data = read_data(input_fp)
+            input_data = json.loads(input_data)
+            # Call the API
+            response = requests.post('http://localhost:20202/process', json={'data': input_data})
+            with open(join(input_dir_path, 'sample_response.txt'), 'w+') as f:
+                f.write(response.text)
+            # convert
+            BASE_PATH = input_dir_path
+            EN_BASE_PATH = join(BASE_PATH, 'en')
+            ES_BASE_PATH = join(BASE_PATH, 'es')
+            ALL_BASE_PATH = join(BASE_PATH, 'all')
+            create_dir_if_not_exist(ALL_BASE_PATH)
+            create_dir_if_not_exist(EN_BASE_PATH)
+            create_dir_if_not_exist(ES_BASE_PATH)
 
-        # Read sample_response.txt
-        with open(join(input_dir_path, 'sample_response.txt')) as f:
-            data = json.loads(f.read())
+            # Read sample_response.txt
+            with open(join(input_dir_path, 'sample_response.txt')) as f:
+                data = json.loads(f.read())
 
-        # Write files (All)
-        with open(join(ALL_BASE_PATH, 'entity.cs'), 'w+') as f:
-            f.write(data['entity.cs'])
-        with open(join(ALL_BASE_PATH, 'event.cs'), 'w+') as f:
-            f.write(data['event.cs'])
-        with open(join(ALL_BASE_PATH, 'clusters.txt'), 'w+') as f:
-            f.write(data['clusters.txt'])
-        with open(join(ALL_BASE_PATH, 'distractors.txt'), 'w+') as f:
-            f.write(data['distrators.txt'])
+            # Write files (All)
+            with open(join(ALL_BASE_PATH, 'entity.cs'), 'w+') as f:
+                f.write(data['entity.cs'])
+            with open(join(ALL_BASE_PATH, 'event.cs'), 'w+') as f:
+                f.write(data['event.cs'])
+            with open(join(ALL_BASE_PATH, 'clusters.txt'), 'w+') as f:
+                f.write(data['clusters.txt'])
+            with open(join(ALL_BASE_PATH, 'distractors.txt'), 'w+') as f:
+                f.write(data['distrators.txt'])
 
-        # Write files (EN)
-        with open(join(EN_BASE_PATH, 'entity.cs'), 'w+') as f:
-            f.write(data['en']['entity.cs'])
-        with open(join(EN_BASE_PATH, 'event.cs'), 'w+') as f:
-            f.write(data['en']['event.cs'])
+            # Write files (EN)
+            with open(join(EN_BASE_PATH, 'entity.cs'), 'w+') as f:
+                f.write(data['en']['entity.cs'])
+            with open(join(EN_BASE_PATH, 'event.cs'), 'w+') as f:
+                f.write(data['en']['event.cs'])
 
-        # Write files (ES)
-        with open(join(ES_BASE_PATH, 'entity.cs'), 'w+') as f:
-            f.write(data['es']['entity.cs'])
-        with open(join(ES_BASE_PATH, 'event.cs'), 'w+') as f:
-            f.write(data['es']['event.cs'])
+            # Write files (ES)
+            with open(join(ES_BASE_PATH, 'entity.cs'), 'w+') as f:
+                f.write(data['es']['entity.cs'])
+            with open(join(ES_BASE_PATH, 'event.cs'), 'w+') as f:
+                f.write(data['es']['event.cs'])
