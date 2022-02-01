30432e9280bc18c511153db93d61afe32b379377
Tuan Manh Lai <boy19ykienandon@yahoo.com.vn>
2022-02-01 08:17:15 -0800

    Add a default value for base_dir

--- run_batch_visualizations.py
+++ run_batch_visualizations.py
@@ -4,8 +4,9 @@ from os.path import isdir, join
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--base_dir', help='Base directory. Each subdir is a complex event',
-                        required=True)
+    parser.add_argument('--base_dir',
+                        help='Base directory. Each subdir is a complex event',
+                        default='/shared/nas/data/m1/tuanml2/kairos_dryrun2')
     args = parser.parse_args()
 
     subdirs = [dn for dn in listdir(args.base_dir) if isdir(join(args.base_dir, dn))]
